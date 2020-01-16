3d6040406aaefcaaea8e37c5d092357282336087
Andre Luis da Cunha <andre.l.cunha@live.com>
2020-01-15 23:37:44 -0300

    Company name parameterized

--- ProvaCandidato.Web/Controllers/HomeController.cs
+++ ProvaCandidato.Web/Controllers/HomeController.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace ProvaCandidato.Controllers
 {
-  public class HomeController : Controller
-  {
-    public ActionResult Index()
+    public class HomeController : Controller
     {
-      return View();
+        public ActionResult Index()
+        {
+            ViewBag.NomeEmpresa = ReadSetting("NomeEmpresa");
+            return View();
+        }
+
+        string ReadSetting(string key)
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                string result = appSettings[key] ?? "Not Found";
+                return result;
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+                return "";
+            }
+        }
     }
-  }
 }
